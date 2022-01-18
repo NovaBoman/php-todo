50318744e5aa09991d2f32815d8d961073de3f29
Nova Boman <nova.boman@chasacademy.se>
2022-01-18 18:34:05 +0100

    add function for showing tasks

--- functions/functions.php
+++ functions/functions.php
@@ -5,9 +5,6 @@ require 'connect.php';
 
 // Create tasks
 
-
-
-
 function createTask($title)
 {
     global $db;
@@ -20,5 +17,22 @@ function createTask($title)
         $createstmt = $db->prepare($createsql);
         $titleSanitized = htmlspecialchars($title);
         $createstmt->execute([':title' => $titleSanitized]);
+    }
+}
+
+// Get all tasks
+
+
+function getTasks()
+{
+    global $db;
+
+    $stmt = $db->query("SELECT * FROM tasks");
+    $tasks = $stmt->fetchAll();
+    foreach ($tasks as $task) {
+        $taskClass = $task->completed == 1 ? "task completed" : "task incomplete";
+        echo '<div class="' . $taskClass . '">' . $task->title . '<a href="/delete.php?id=' . $task->id . '">Delete</a>
+        <a href="/edit.php?id=' . $task->id . '">Edit</a>
+        <a href="/completed.php?id=' . $task->id . '&completed=' . $task->completed . '">Done</a></div>';
     }
 }
