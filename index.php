dc3950eaa86e00967bd56f3b926efb0cbca863d7
Nova Boman <nova.boman@chasacademy.se>
2022-01-27 18:08:53 +0100

    update page titles

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ require 'functions/functions.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>U04</title>
+    <title>u04 - ToDo-app</title>
 </head>
 
 <body>
